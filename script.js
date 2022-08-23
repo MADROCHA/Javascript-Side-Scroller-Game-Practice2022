3b6cc39ff4b208839f2fdaec7f9bfbb7c663d8f3
MADROCHA <madlarocha@gmail.com>
2022-08-23 03:39:32 +0200

    _script.js_InputHnadlerClass_manageKeyboardInputs_array.indexOf_push.splice//_SideScrollGame_

--- script.js
+++ script.js
@@ -4,12 +4,35 @@ window.addEventListener('load', function(){
     canvas.width = 800;
     canvas.height = 720;
     
-    
+
     /* CLASSES */
     class InputHandler {
-        
+        constructor(){
+            this.keys = [];
+            window.addEventListener('keydown', e => {
+                /* console.log(e.key); */
+                if ((e.key === 's' ||
+                    e.key === 'w' ||
+                    e.key === 'a' ||
+                    e.key === 'd' )
+                    && this.keys.indexOf(e.key) === -1){
+                    this.keys.push(e.key);
+                }
+                console.log(e.key, this.keys);
+            }); 
+            window.addEventListener('keyup', e => {
+                if (e.key === 's' ||
+                    e.key === 'w' ||
+                    e.key === 'a' ||
+                    e.key === 'd' ) {
+                    this.keys.splice(this.keys.indexOf(e.key), 1);
+                }
+                console.log(e.key, this.keys);
+            }); 
+        }
     }
     class Player {
+
     }
     class Background {
         
@@ -27,6 +50,7 @@ window.addEventListener('load', function(){
     function displayStatusText(){
 
     }
+    const input = new InputHandler();
     function animate(){
 
     }
